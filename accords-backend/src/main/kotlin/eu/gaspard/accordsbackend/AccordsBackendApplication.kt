c062045aa15adc2351bc7a454db684780259bbdc
GaspardPO <gaspard.pointeau@gmail.com>
2024-06-04 10:57:27 +0200

    build - backend - initialize springbootproject

---
+++ accords-backend/src/main/kotlin/eu/gaspard/accordsbackend/AccordsBackendApplication.kt
@@ -0,0 +1,11 @@
+package eu.gaspard.accordsbackend
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class AccordsBackendApplication
+
+fun main(args: Array<String>) {
+    runApplication<AccordsBackendApplication>(*args)
+}
